eb77c770de322006f4d80d800c4df6b521914b8e
siriusx711 <53624147+siriusx711@users.noreply.github.com>
2021-08-06 20:21:04 -0500

    logo e iconos

--- pos.js
+++ pos.js
@@ -1,28 +1,3 @@
-/*
-var options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
-};
-
-function success(pos) {
-  var crd = pos.coords;
-
-  console.log('Your current position is:');
-  console.log('Latitude : ' + crd.latitude);
-  console.log('Longitude: ' + crd.longitude);
-  console.log('More or less ' + crd.accuracy + ' meters.');
-
-  let showCord = document.getElementById('label');
-  showCord.innerHTML = crd.latitude + " " + crd.longitude;
-};
-
-function error(err) {
-  console.warn('ERROR(' + err.code + '): ' + err.message);
-};
-
-navigator.geolocation.getCurrentPosition(success, error, options);
-*/
 const d = document,
 n = navigator;
 
