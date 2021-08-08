bcfb6f9b24be349d442ac97a9202f7a67993068c
siriusx711 <53624147+siriusx711@users.noreply.github.com>
2021-08-07 23:52:30 -0500

    corrigiendo error de lat lang

--- pos.js
+++ pos.js
@@ -1,7 +1,7 @@
 
-var id, target, option, mapa, intervalo;
+var id, target, option, mapa, intervalo, intervalo2;
 const d = document;
-var crd;
+
 
 function getGeolocation(id){
 
@@ -18,21 +18,34 @@ function getGeolocation(id){
   }
 
   const success = (position) => {
-
-    crd = position.coords;
+    var crd = position.coords;
     let showCord = document.getElementById('geolocation');
     showCord.innerHTML = "Lat: " + crd.latitude + "<br>" + "Long: " + crd.longitude + "<br>" + "Prec: " + crd.accuracy;
-    //console.log(position);
 
-      if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
-        console.log('Congratulation, you reach the target');
-        navigator.geolocation.clearWatch(id);
-      }
+    if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
+      console.log('Congratulation, you reach the target');
+      navigator.geolocation.clearWatch(id);
+    }
 
     mapa = d.getElementById('mapa');
     mapa.innerHTML=`<a href="https://www.google.com/maps/@${crd.latitude},${crd.longitude},20z" target="_blank" rel="noopener">Ver en Google Maps</a>`;
+    // colocar el codigo del intervalo
+    var ult = document.getElementById('ult');
+    var act = document.getElementById('act');
+    var pActual = [crd.latitude,crd.longitude];
 
+    intervalo2 = setInterval(function () {
+      var pActual = [crd.latitude,crd.longitude];
+      console.log("Actual:"+"Lat: "+ pActual[0]+ " Lng: " + pActual[1]);
+      intervalo;
+      return (act.innerHTML = "Actual posicion: " + "Lat:" + pActual[0] + " Lng: " + pActual[1] + "<br>");
+    },5000);
 
+    intervalo = setInterval(function () {
+      var pUltima = [crd.latitude,crd.longitude];
+      console.log("Última: "+"Lat: "+ pUltima[0]+ " Lng: " + pUltima[1]);
+      return (ult.innerHTML = "Última posicion: " + "Lat:" + pUltima[0] + " Lng: " + pUltima[1] + "<br>");
+    },10000);
 
   }
 
@@ -45,26 +58,5 @@ function getGeolocation(id){
 
 }
 
-// colocar el codigo del intervalo
-var ult = document.getElementById('ult');
-var act = document.getElementById('act');
 
-
-function init(){
-  var pActual = [crd.latitude,crd.longitude];
-  intervalo = setInterval(function () {
-    var pUltima = [crd.latitude,crd.longitude];
-    console.log("Última: "+"Lat: "+ pUltima[0]+ " Lng: " + pUltima[1]);
-    return (ult.innerHTML = "Última posicion: " + "Lat:" + pUltima[0] + " Lng: " + pUltima[1] + "<br>");
-  },10000);
-
-  intervalo2 = setInterval(function () {
-    var pActual = [crd.latitude,crd.longitude];
-    console.log("Actual:"+"Lat: "+ pActual[0]+ " Lng: " + pActual[1]);  //(dir.innerHTML = "Ultima posicion: " + pUltima[0,1] + "<br>" + "Actual: " + pActual[0,1]);
-    intervalo;
-    return (act.innerHTML = "Actual posicion: " + "Lat:" + pActual[0] + " Lng: " + pActual[1] + "<br>");
-  },1000);
-
-}
-
-window.addEventListener("load", init, false);
+//window.addEventListener("load", intervalo2, false);
