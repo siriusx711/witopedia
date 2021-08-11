bab6db77152ad00feb7c9a1cca398065d90c7f3c
siriusx711 <53624147+siriusx711@users.noreply.github.com>
2021-08-11 11:44:11 -0500

    cambio de variables

--- lastpos.js
+++ lastpos.js
@@ -2,27 +2,27 @@
 var ultima = document.getElementById('ult');
 var actual = document.getElementById('act');
 
-var options = {
+var options2 = {
   enableHighAccuracy: true,
   timeout: 5000,
   maximumAge: 10000,
 };
 
-function success(pos) {
-  var crd = pos.coords;
+function success2(pos2) {
+  var crd2 = pos2.coords;
   var lst_lat, lst_lng;
-  lst_lat = ultima.innerHTML = 'Last Latitude : ' + crd.latitude;
-  lst_lng = actual.innerHTML = 'Last Longitude : ' + crd.longitude;
+  lst_lat = ultima.innerHTML = 'Last Latitude : ' + crd2.latitude;
+  lst_lng = actual.innerHTML = 'Last Longitude : ' + crd2.longitude;
 
   console.log('Your current position is:');
-  console.log('Latitude : ' + crd.latitude);
-  console.log('Longitude: ' + crd.longitude);
-  console.log('More or less ' + crd.accuracy + ' meters.');
+  console.log('Latitude : ' + crd2.latitude);
+  console.log('Longitude: ' + crd2.longitude);
+  console.log('More or less ' + crd2.accuracy + ' meters.');
 };
 
-function error(err) {
-  console.warn('ERROR(' + err.code + '): ' + err.message);
+function error2(err2) {
+  console.warn('ERROR(' + err2.code + '): ' + err2.message);
 };
 
-navigator.geolocation.watchPosition(success, error, options);
+navigator.geolocation.watchPosition(success2, error2, options2);
 //navigator.geolocation.getCurrentPosition(success, error, options);
