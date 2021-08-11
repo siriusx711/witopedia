766ccfbcfec4f372d13322cc44fee7f4ba288ab6
siriusx711 <53624147+siriusx711@users.noreply.github.com>
2021-08-11 13:44:41 -0500

    localstorage

--- lastpos.js
+++ lastpos.js
@@ -1,28 +1,31 @@
 // window.addEventListener("load", intervalo, false);
-var ultima = document.getElementById('ult');
-var actual = document.getElementById('act');
-
-var options2 = {
-  enableHighAccuracy: false,
-  timeout: 5000,
-  maximumAge: 10000,
-};
-
-function success2(pos2) {
-  var crd2 = pos2.coords;
-  var lst_lat, lst_lng;
-  lst_lat = ultima.innerHTML = 'Last Latitude : ' + crd2.latitude;
-  lst_lng = actual.innerHTML = 'Last Longitude : ' + crd2.longitude;
-
-  console.log('Your current position is:');
-  console.log('Latitude : ' + crd2.latitude);
-  console.log('Longitude: ' + crd2.longitude);
-  console.log('More or less ' + crd2.accuracy + ' meters.');
-};
-
-function error2(err2) {
-  console.warn('ERROR(' + err2.code + '): ' + err2.message);
-};
-
-navigator.geolocation.watchPosition(success2, error2, options2);
+// var ultima = document.getElementById('ult');
+// var actual = document.getElementById('act');
+//
+// var options2 = {
+//   enableHighAccuracy: false,
+//   timeout: 5000,
+//   maximumAge: 10000,
+// };
+//
+// function success2(pos2) {
+//   var crd2 = pos2.coords;
+//   var lst_lat, lst_lng;
+//   lst_lat = ultima.innerHTML = 'Last Latitude : ' + crd2.latitude;
+//   lst_lng = actual.innerHTML = 'Last Longitude : ' + crd2.longitude;
+//
+//   console.log('Your current position is:');
+//   console.log('Latitude : ' + crd2.latitude);
+//   console.log('Longitude: ' + crd2.longitude);
+//   console.log('More or less ' + crd2.accuracy + ' meters.');
+// };
+//
+// function error2(err2) {
+//   console.warn('ERROR(' + err2.code + '): ' + err2.message);
+// };
+//
+// navigator.geolocation.watchPosition(success2, error2, options2);
 //navigator.geolocation.getCurrentPosition(success, error, options);
+
+
+//window.addEventListener("load", intervalo, false);
--- pos.js
+++ pos.js
@@ -1,7 +1,9 @@
-
 var id, target, option, mapa;
 const d = document;
 var showCord = d.getElementById('geolocation');
+var latSt, lngSt, crd;
+var ultima = document.getElementById('ult');
+var actual = document.getElementById('act');
 
 function getGeolocation(id)
 {
@@ -20,9 +22,9 @@ function getGeolocation(id)
 
   const success = (position) => {
 
-    var crd = position.coords;
+    crd = position.coords;
     showCord.innerHTML = "Lat: " + crd.latitude + "<br>" + "Long: " + crd.longitude + "<br>" + "Prec: " + crd.accuracy;
-    console.log("Lat: " + crd.latitude + " Long: " + crd.longitude);
+
     if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
       console.log('Congratulation, you reach the target');
       navigator.geolocation.clearWatch(id);
@@ -30,13 +32,37 @@ function getGeolocation(id)
 
     mapa = d.getElementById('mapa');
     mapa.innerHTML=`<a href="https://www.google.com/maps/@${crd.latitude},${crd.longitude},20z" target="_blank" rel="noopener">Ver en Google Maps</a>`;
-
   }
 
   const error = (er) => {
     $id.innerHTML = `<p><mark>Error ${er.code}: ${er.message}</mark></p>`;
     console.log(`Error ${er.code}:${er.message}`);
   }
-
   id = navigator.geolocation.watchPosition(success, error, options);
 }
+
+var lat = "Lat:";
+var lng = "Lng:";
+var borrarLatSt, borrarLngSt;
+
+function setCoords(){
+  latSt = window.localStorage.setItem("Lat:", crd.latitude);
+  lngSt = window.localStorage.setItem("Lng:", crd.longitude);
+}
+
+function getCoords(){
+  var intervalo;
+  intervalo = setInterval(aletF,5000);
+}
+
+function aletF(){
+  console.log( "Recuperado: " + window.localStorage.getItem(lat) );
+  console.log( "Recuperado: " + window.localStorage.getItem(lng) );
+  ultima.innerHTML = window.localStorage.getItem(lat);
+  actual.innerHTML = window.localStorage.getItem(lng);
+  borrarLatSt = window.localStorage.removeItem(lat);
+  borrarLngSt = window.localStorage.removeItem(lng);
+  setCoords();
+}
+
+getCoords();
