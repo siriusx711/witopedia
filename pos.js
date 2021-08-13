dab92516ee0e10ce1174a06caeaab4d489c44048
siriusx711 <53624147+siriusx711@users.noreply.github.com>
2021-08-13 00:15:51 -0500

    speed

--- pos.js
+++ pos.js
@@ -21,13 +21,11 @@ function getGeolocation(id)
 
   const success = (position) => {
     crd = position.coords;
-    showCord.innerHTML = "Lat: " + crd.latitude + "<br>" + "Long: " + crd.longitude + "<br>" + "Prec: " + crd.accuracy;
+    showCord.innerHTML = "Lat: " + crd.latitude + "<br>" + "Long: " + crd.longitude + "<br>" + "Velocidad: "+ crd.speed + "<br>" + "Prec: " + crd.accuracy;
     if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
       console.log('Congratulation, you reach the target');
       navigator.geolocation.clearWatch(id);
     }
-    //mapa = d.getElementById('mapa');
-    //mapa.innerHTML=`<a href="https://www.google.com/maps/@${crd.latitude},${crd.longitude},20z" target="_blank" rel="noopener">Ver en Google Maps</a>`;
   }
 
   const error = (er) => {
@@ -115,6 +113,7 @@ function initMap() {
     });
   }
   map.setOptions({ styles: styles["hide"] });
+  
 }
 
 const styles = {
